4096524ad78c9d82290c57cb9c031233e84416dd
qodbtn41 <qodbtn41@naver.com>
2021-06-03 01:05:47 +0900

    [6월 1주차][스프링 입문] 코드 짧게 쓰기

--- 스프링_입문/hello_spring/src/main/java/hello_spring/hello_spring/service/MemberService.java
+++ 스프링_입문/hello_spring/src/main/java/hello_spring/hello_spring/service/MemberService.java
@@ -17,8 +17,7 @@ public class MemberService {
    */
   public long join(Member member) {
     // 같은 이름이 있는 중복 회원은 안된다.
-    Optional<Member> result = memberRepository.findByName(member.getName());
-    result.ifPresent(m -> {
+    memberRepository.findByName(member.getName()).ifPresent(m -> {
       throw new IllegalStateException("이미 존재하는 회원입니다.");
     });
 
