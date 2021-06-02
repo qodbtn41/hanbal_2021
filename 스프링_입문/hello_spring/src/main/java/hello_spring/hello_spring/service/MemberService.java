2205897e6235fccb5ba75b524ad9c8f567feac46
qodbtn41 <qodbtn41@naver.com>
2021-06-03 01:13:04 +0900

    [6월 1주차][스프링 입문] 자주 쓰는 구문은 메서드로 추출한다.

--- 스프링_입문/hello_spring/src/main/java/hello_spring/hello_spring/service/MemberService.java
+++ 스프링_입문/hello_spring/src/main/java/hello_spring/hello_spring/service/MemberService.java
@@ -1,5 +1,6 @@
 package hello_spring.hello_spring.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import hello_spring.hello_spring.domain.Member;
@@ -17,11 +18,19 @@ public class MemberService {
    */
   public long join(Member member) {
     // 같은 이름이 있는 중복 회원은 안된다.
+    validateDuplicateMember(member);
+    memberRepository.save(member);
+    return member.getId();
+  }
+
+  /**
+   * 동일한 이름의 회원이 있는지 검증한다.
+   * 
+   * @param member
+   */
+  private void validateDuplicateMember(Member member) {
     memberRepository.findByName(member.getName()).ifPresent(m -> {
       throw new IllegalStateException("이미 존재하는 회원입니다.");
     });
-
-    memberRepository.save(member);
-    return member.getId();
   }
 }
