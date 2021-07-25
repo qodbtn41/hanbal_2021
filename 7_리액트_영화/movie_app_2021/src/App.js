8a6c6534d5edf3e10c8820fdcd1bdbe4df58f06c
qodbtn41 <qodbtn41@naver.com>
2021-07-26 06:35:48 +0900

    props proptypes

--- 7_리액트_영화/movie_app_2021/src/App.js
+++ 7_리액트_영화/movie_app_2021/src/App.js
@@ -1,13 +1,31 @@
 import Potato from "./Potato";
+import PropTypes from "prop-types";
 
 function Food({ name }) {
-  return <h1>{name}</h1>;
+  return <h1>I Like {name}</h1>;
 }
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
+const foodILike = [
+  { id: 1, name: "김치", image: "", rating: 5 },
+  { id: 2, name: "삼겹살", image: "", rating: 5 },
+  { id: 3, name: "두부", image: "", rating: 5 },
+  { id: 4, name: "감자", image: "", rating: 5 },
+];
+
+function renderFood(dish) {
+  return <Food key={dish.id} name={dish.name} picture={dish.image} />;
+}
+
 function App() {
   return (
     <div className="App">
       Hello!!! <Potato />
-      I Like <Food name="김치" />
+      {foodILike.map(renderFood)}
     </div>
   );
 }
