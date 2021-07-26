5143b27ac49bcc798623880866cdb55a2410507c
qodbtn41 <qodbtn41@naver.com>
2021-07-26 21:20:38 +0900

    생명주기

--- 7_리액트_영화/movie_app_2021/src/App.js
+++ 7_리액트_영화/movie_app_2021/src/App.js
@@ -1,33 +1,46 @@
 import Potato from "./Potato";
 import PropTypes from "prop-types";
+import React from "react";
 
-function Food({ name }) {
-  return <h1>I Like {name}</h1>;
-}
-
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-const foodILike = [
-  { id: 1, name: "김치", image: "", rating: 5 },
-  { id: 2, name: "삼겹살", image: "", rating: 5 },
-  { id: 3, name: "두부", image: "", rating: 5 },
-  { id: 4, name: "감자", image: "", rating: 5 },
-];
-
-function renderFood(dish) {
-  return <Food key={dish.id} name={dish.name} picture={dish.image} />;
-}
-
-function App() {
-  return (
-    <div className="App">
-      Hello!!! <Potato />
-      {foodILike.map(renderFood)}
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+  }
+  state = {
+    count: 0,
+  };
+  add = () => {
+    console.log("add");
+    this.setState((current) => ({
+      count: current.count + 1,
+    }));
+  };
+  minus = () => {
+    console.log("minus");
+    this.setState((current) => ({
+      count: this.state.count - 1,
+    }));
+  };
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+  componentDidUpdate() {
+    console.log("componentDidUpdate");
+  }
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
