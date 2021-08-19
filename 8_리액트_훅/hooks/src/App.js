bc7295aa2ddd9c2fa9d1b9593d4d008f3547c041
qodbtn41 <qodbtn41@naver.com>
2021-08-20 05:39:30 +0900

    UseInput 듣는중

--- 8_리액트_훅/hooks/src/App.js
+++ 8_리액트_훅/hooks/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const useInput = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+  const onChange = (event) => {
+    console.log(event.target);
+  };
+  return { value, onChange };
+};
 
 function App() {
+  const name = useInput("Mr.");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello </h1>
+      <input placeholder="Name" value={name.value} onChange={name.onChange} />
+      <input placeholder="Name" {...name} />
     </div>
   );
 }
+// 분리된 파일, 컴포넌트 내의 함수가 아니라 바깥의 함수를 쓸수가 있다.
+// 무한한 확장성? 과 공통화(사전작업된 기능). 필요할때 붙여서 쓰는 느낌
+// 만들어야 되는것만 만들고 나머지는 갖다 쓰는 느낌
 
 export default App;
