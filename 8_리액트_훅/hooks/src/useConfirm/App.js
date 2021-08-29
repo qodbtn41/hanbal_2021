846073b61bbe9b7870a5272c717ca48c7776dcfa
qodbtn41 <qodbtn41@naver.com>
2021-08-29 23:15:34 +0900

    useConfirm

---
+++ 8_리액트_훅/hooks/src/useConfirm/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const usePreventLeave = () => {
+  const listener = (event) => {
+    event.preventDefault();
+    event.returnValue = "";
+  };
+  const enablePrevent = () => window.addEventListener("beforeunload", listener);
+  const disablePrevent = () => window.removeEventListener("beforeunload", listener);
+  return { enablePrevent, disablePrevent };
+};
+function App() {
+  const { enablePrevent, disablePrevent } = usePreventLeave();
+  return (
+    <div className="App">
+      <button onClick={enablePrevent}>Protect</button>
+      <button onClick={disablePrevent}>Unprotect</button>
+    </div>
+  );
+}
+
+export default App;
