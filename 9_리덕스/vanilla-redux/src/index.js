fd9364bd1c435140bb6889426d44157b3f7db93b
qodbtn41 <qodbtn41@naver.com>
2021-09-27 02:19:20 +0900

    리덕스 #1.4

--- 9_리덕스/vanilla-redux/src/index.js
+++ 9_리덕스/vanilla-redux/src/index.js
@@ -4,17 +4,20 @@ const plus = document.getElementById("plus");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+const PLUS = "PLUS";
+const MINUS = "MINUS";
 // reducer는 데이터를 변경하는 함수다.
 // 반환하는 값이 그 데이터가 된다.
 // 이 안에서만 데이터를 변경할 수 있다.
-const countModifier = (state = 0, action) => {
-  console.log(action);
-  if (action.type === "PLUS") {
-    state++;
-  } else if (action.type === "MINUS") {
-    state--;
+const countModifier = (count = 0, action) => {
+  switch (action.type) {
+    case PLUS:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
   }
-  return state;
 };
 
 const countStore = createStore(countModifier);
@@ -25,9 +28,9 @@ const onChange = () => {
 countStore.subscribe(onChange);
 
 plus.addEventListener("click", () => {
-  countStore.dispatch({ type: "PLUS" });
+  countStore.dispatch({ type: PLUS });
 });
 
 minus.addEventListener("click", () => {
-  countStore.dispatch({ type: "MINUS" });
+  countStore.dispatch({ type: MINUS });
 });
